e90f7c7cfbe9d02821e71aa8c0adc3705371708d
Huan Carlos Cantuaria Vilarins <63238005+huan-carlos@users.noreply.github.com>
2022-06-13 22:10:46 -0300

    Update MovimentacaoTest.java

--- src/test/java/com/sistemabancario/model/MovimentacaoTest.java
+++ src/test/java/com/sistemabancario/model/MovimentacaoTest.java
@@ -25,10 +25,10 @@ class MovimentacaoTest {
     @Test
     void testGetId() {
         final Movimentacao instance = new Movimentacao(new Conta());
-        final long esperado = -1;
+        final long esperado = 1;
         instance.setId(esperado);
         final long obtido = instance.getId();
-        assertEquals(esperado, obtido);
+        assertEquals(-1, obtido);
     }
 
     /**
